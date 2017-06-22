bcb8c5f65076dbd05598fbb1ee50bf7617d9b80a
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-06-22 16:59:52 +0900

    helloworld for material-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<p>Hello, world!</p>, document.getElementById('react-root'));
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  // https://blog.rudolph-miller.com/2016/01/11/theme-of-material-ui/
+  fontFamily: "'Roboto', 'Noto Sans JP', 'sans-serif'",
+});
+
+const App = () => <RaisedButton label="Default" />;
+
+ReactDOM.render(
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('react-root')
+);
 registerServiceWorker();
