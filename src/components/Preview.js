bea018e958ab7f0243a6e3a16f61a2c91ed8174c
Sho Iizuka <sho.i518@gmail.com>
2017-06-23 14:39:35 +0900

    eval

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,19 +3,39 @@ import { Card, CardActions, CardMedia, CardText } from 'material-ui/Card';
 
 import FlatButton from 'material-ui/FlatButton';
 import IconFileUpload from 'material-ui/svg-icons/file/file-upload';
+import IconStar from 'material-ui/svg-icons/toggle/star';
+import IconStarBorder from 'material-ui/svg-icons/toggle/star-border';
 import React from 'react';
 import Slider from 'material-ui/Slider';
-import TextField from 'material-ui/TextField';
+
+const styles = {
+  stars: {
+    textAlign: 'center',
+  },
+  largeIcon: {
+    width: "72px",
+    height: "72px"
+  },
+  card: {
+    maxWidth: "970px",
+    margin: "0 auto"
+  }
+}
 
 export default class Preview extends React.Component {
   render = () =>
-    <Card>
+    <Card style={styles.card}>
       <CardMedia>
         <img src="images/IMG_20170614_181803.jpg" alt="" />
       </CardMedia>
       <CardText>
-        <Slider min={1} max={4} step={1} value={4} />
-        <TextField hintText="投稿者名" fullWidth={true} />
+        {/*スライダーじゃなくて☆をタッチでいいような気がしてきた*/}
+        {/*<Slider min={1} max={4} step={1} value={4} />*/}
+        {/*http://www.irasutoya.com/2015/08/5.html*/}
+        <div style={styles.stars}>
+          <IconStar style={styles.largeIcon} />
+          <IconStarBorder style={styles.largeIcon} />
+        </div>
       </CardText>
       <CardActions>
         <FlatButton primary label="投稿する" icon={<IconFileUpload />} />
