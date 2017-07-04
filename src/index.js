187473a934d46c00f6ada9764b3aa11e994cce61
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-07-04 16:13:16 +0900

    sort imports

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // @flow
-import './index.css';
-
-import App from './components/App';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
