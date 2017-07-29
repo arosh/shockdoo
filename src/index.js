1418e8c5ac7b9e1581f81802eabf7411d4678b82
Sho Iizuka <sho.i518@gmail.com>
2017-07-30 00:52:19 +0900

    setup font

--- src/index.js
+++ src/index.js
@@ -13,18 +13,13 @@ import store from './flux/store';
 import 'flexboxgrid/css/flexboxgrid.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'typeface-roboto';
-import 'typeface-noto-sans';
 import './index.css';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const muiTheme = getMuiTheme({
-  // 日本語用にNotoフォントを入れておく
-  // https://blog.rudolph-miller.com/2016/01/11/theme-of-material-ui/
-  fontFamily: "'Roboto', 'Noto Sans JP', 'sans-serif'",
-});
+const muiTheme = getMuiTheme({});
 
 ReactDOM.render(
   <Provider store={store}>
