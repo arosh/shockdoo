d207aa756cfd801934a179f6e95da976efcc2aef
Sho Iizuka <sho.i518@gmail.com>
2017-07-31 03:16:46 +0900

    Upgrade resolution

--- favicon/build.sh
+++ favicon/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-convert -density 200 -flatten favicon/caret.pdf png:- | convert -crop 600x600+527+25 png:- -define icon:auto-resize public/favicon.ico
+convert -density 400 -flatten favicon/caret.pdf png:- | convert -crop 1200x1200+1054+50 png:- -define icon:auto-resize public/favicon.ico
