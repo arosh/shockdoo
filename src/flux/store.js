cdb2184ea8095b5ef12672ccc272a4b6d964d425
Sho Iizuka <iizuka.sho.ij4@is.naist.jp>
2017-10-24 18:15:21 +0900

    modify database.rules.json

--- src/flux/store.js
+++ src/flux/store.js
@@ -15,26 +15,24 @@ const initialState: State = {
   drawerOpened: false,
 };
 
-const rootReducer = handleActions(
-  {
-    SIGN_IN: (state, { payload }) => ({
-      ...state,
-      logged: true,
-      userId: payload.userId,
-    }),
-    SIGN_OUT: state => ({
-      ...state,
-      logged: false,
-    }),
-    TOGGLE_DRAWER: (state, { payload }) => ({
-      ...state,
-      drawerOpened: payload.open,
-    }),
-  },
-  initialState
-);
-
 export default createStore(
-  rootReducer,
+  handleActions(
+    {
+      SIGN_IN: (state, { payload }) => ({
+        ...state,
+        logged: true,
+        userId: payload.userId,
+      }),
+      SIGN_OUT: state => ({
+        ...state,
+        logged: false,
+      }),
+      TOGGLE_DRAWER: (state, { payload }) => ({
+        ...state,
+        drawerOpened: payload.open,
+      }),
+    },
+    initialState
+  ),
   composeWithDevTools(applyMiddleware(thunk))
 );
