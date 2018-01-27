c86ce01097a05d46276d7cac32677a227b0d07be
Sho Iizuka <sho.i518@gmail.com>
2018-01-27 22:02:37 +0900

    multipart/form-data is unavailable

--- functions/index.ts
+++ functions/index.ts
@@ -21,65 +21,82 @@ const authenticate = (req, res, next) => {
   });
 };
 
-exports.set_user_name = functions.https.onRequest(async (req: any, res) => {
-  authenticate(req, res, async () => {
-    const MAX_LENGTH = 20;
-    const { uid } = req;
-    const userName: string = req.body.userName;
-    if (!userName || userName.length > MAX_LENGTH) {
-      res.status(400).send('Bad Request');
-      return;
-    }
-    await logic.setUserName(uid, userName);
-    res.status(200).json({ ok: true });
-  });
-});
-
-exports.add_photo = functions.https.onRequest(async (req: any, res) => {
-  console.log('authenticate');
-  authenticate(req, res, async () => {
-    console.log('in authenticate');
-    const { uid } = req;
-    const form = new formidable.IncomingForm();
-    form.maxFieldsSize = MAX_FIELDS_SIZE;
-    console.log('form.parse');
-    form.parse(req, async (err, fields, files) => {
-      if (!fields.star || !files.image) {
+exports.set_user_name = functions.https.onRequest((req: any, res) => {
+  return new Promise((resolve, reject) => {
+    authenticate(req, res, async () => {
+      const MAX_LENGTH = 20;
+      const { uid } = req;
+      const userName: string = req.body.userName;
+      if (!userName || userName.length > MAX_LENGTH) {
         res.status(400).send('Bad Request');
         return;
       }
-      const star = parseInt(fields.star, 10);
-      if (!(1 <= star && star <= 5)) {
-        res.status(400).send('Bad Request');
-        return;
-      }
-      const file = files.image;
-      const imagePath = file.path;
-      const mimeType = file.type;
-      if (!mimeType.startsWith('image/')) {
-        res.status(400).send('Bad Request');
-        return;
-      }
-      const photoID = await logic.addPhoto(uid, star, imagePath, mimeType);
-      res.status(200).json({ photoID });
+      await logic.setUserName(uid, userName);
+      res.status(200).json({ ok: true });
+      resolve();
     });
   });
 });
 
-exports.add_like = functions.https.onRequest(async (req: any, res) => {
-  authenticate(req, res, async () => {
-    const uid = req.uid;
-    const photoID = req.body.photoID;
-    const ok = await logic.addLike(uid, photoID);
-    res.status(200).json({ ok });
+exports.add_photo = functions.https.onRequest((req: any, res) => {
+  return new Promise((resolve, reject) => {
+    authenticate(req, res, () => {
+      const { uid } = req;
+      const form = new formidable.IncomingForm();
+      form.maxFieldsSize = MAX_FIELDS_SIZE;
+      form.parse(req, async (err, fields, files) => {
+        const f = async () => {
+          console.log('form.parse');
+          if (!fields.star || !files.image) {
+            console.log(`fields.star = ${fields.star}`);
+            console.log(`files.image = ${files.image}`);
+            res.status(400).send('Bad Request');
+            return;
+          }
+          const star = parseInt(fields.star, 10);
+          if (!(1 <= star && star <= 5)) {
+            console.log(`fields.star = ${fields.star}`);
+            res.status(400).send('Bad Request');
+            return;
+          }
+          const file = files.image;
+          const imagePath = file.path;
+          const mimeType = file.type;
+          if (!mimeType.startsWith('image/')) {
+            console.log(`mimeType = ${mimeType}`);
+            res.status(400).send('Bad Request');
+            return;
+          }
+          const photoID = await logic.addPhoto(uid, star, imagePath, mimeType);
+          res.status(200).json({ photoID });
+        };
+        await f();
+        resolve();
+      });
+    });
   });
 });
 
-exports.remove_like = functions.https.onRequest(async (req: any, res) => {
-  authenticate(req, res, async () => {
-    const uid = req.uid;
-    const photoID = req.body.photoID;
-    const ok = await logic.removeLike(uid, photoID);
-    res.status(200).json({ ok });
+exports.add_like = functions.https.onRequest((req: any, res) => {
+  return new Promise((resolve, reject) => {
+    authenticate(req, res, async () => {
+      const { uid } = req;
+      const photoID = req.body.photoID;
+      const ok = await logic.addLike(uid, photoID);
+      res.status(200).json({ ok });
+      resolve();
+    });
+  });
+});
+
+exports.remove_like = functions.https.onRequest((req: any, res) => {
+  return new Promise((resolve, reject) => {
+    authenticate(req, res, async () => {
+      const { uid } = req;
+      const photoID = req.body.photoID;
+      const ok = await logic.removeLike(uid, photoID);
+      res.status(200).json({ ok });
+      resolve();
+    });
   });
 });
